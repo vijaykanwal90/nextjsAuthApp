841e58ba611a2898b901a284eb4a881ec423a055
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-01-28 15:17:13 +0530

    forgot password feature

---
+++ src/app/forgotpassword/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import axios from axios;
+import React, {useEffect} from "react";
+import {useRouter} from "next/navigation";
+require('dotenv').config()
+import Link from "next/link";
+import { toast } from "react-hot-toast";
+
+export default function forgotPassword(){
+
+
+
+const forgot = async ()=>{
+
+}
+
+return (
+
+    <div>
+
+    </div>
+)
+}
